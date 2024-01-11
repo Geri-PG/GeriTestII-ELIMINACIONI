f184e43e2b35a3309ea0df3d95dc096ee6666cf0
Geri-PG <123660669+Geri-PG@users.noreply.github.com>
2024-01-11 23:12:41 +0100

    some changes

--- src/components/form/FormInput.js
+++ src/components/form/FormInput.js
@@ -1,38 +1,41 @@
-import React, {useEffect, useState} from "react";
-import classes from './FormInput.module.css';
+import React, { useEffect, useState } from "react";
+import classes from "./FormInput.module.css";
 
-export default function FormInput({ value, title, placeholder, validationErrors, onChange }) {
-    const [inputValue, setInputValue] = useState('');
+export default function FormInput({
+  value,
+  title,
+  placeholder,
+  validationErrors,
+  onChange,
+}) {
+  const [inputValue, setInputValue] = useState("");
 
-    const handleValueChange = (event) => {
-        const newValue = event.target.value;
-        setInputValue(newValue);
-        onChange(newValue);
-    };
+  const handleValueChange = (event) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+    onChange(newValue);
+  };
 
-    useEffect(() => {
-        if (value) {
-            setInputValue(value);
-        }
-    }, []);
+  useEffect(() => {
+    if (value) {
+      setInputValue(value);
+    }
+  }, []);
 
-
-    return (
-        <div className={classes["form-group"]}>
-            <label htmlFor={title.toLowerCase()}>{title}</label>
-            <input
-                type="text"
-                name={title.toLowerCase()}
-                value={inputValue}
-                onChange={handleValueChange}
-                placeholder={placeholder}
-                className={validationErrors.length > 0 ? classes['error-input'] : ''}
-            />
-            {validationErrors ?
-                <p className={classes['error-message']}>
-                    {validationErrors[0]}
-                </p>
-                : null}
-        </div>
-    );
+  return (
+    <div className={classes["form-group"]}>
+      <label htmlFor={title.toLowerCase()}>{title}</label>
+      <input
+        type="text"
+        name={title.toLowerCase()}
+        value={inputValue}
+        onChange={handleValueChange}
+        placeholder={placeholder}
+        className={validationErrors.length > 0 ? classes["error-input"] : ""}
+      />
+      {validationErrors ? (
+        <p className={classes["error-message"]}>{validationErrors[0]}</p>
+      ) : null}
+    </div>
+  );
 }
